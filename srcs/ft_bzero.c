e92515fd89a45530df5e650aa66af948b0c92017
Alexandre DE TEMMERMAN <ade-temm@e1r13p11.42.fr>
2019-10-07 15:33:06 +0200

    ft_bzero.c

--- srcs/ft_bzero.c
+++ srcs/ft_bzero.c
@@ -1,23 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft.h                                               :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ade-temm <ade-temm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/07 14:24:41 by ade-temm          #+#    #+#             */
-/*   Updated: 2019/10/07 15:15:40 by ade-temm         ###   ########.fr       */
+/*   Created: 2019/10/07 15:14:49 by ade-temm          #+#    #+#             */
+/*   Updated: 2019/10/07 15:28:01 by ade-temm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_H
-# define FT_H
+#include "../includes/ft.h"
 
-# include <string.h>
-# include <unistd.h>
-# include <stdlib.h>
+void	ft_bzero(void *s, size_t n)
+{
+	size_t i;
 
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
-
-#endif
+	i = 0;
+	while (i < n)
+	{
+		*(char*)s = 0;
+		s++;
+		i++;
+	}
+}
