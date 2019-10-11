3a1039884c67d799e4835bcf87baa79e9812e96f
Alexandre DE TEMMERMAN <ade-temm@e1r10p16.42.fr>
2019-10-11 17:36:30 +0200

    putchar, putstr, putendl fd

--- includes/ft.h
+++ includes/ft.h
@@ -6,7 +6,7 @@
 /*   By: ade-temm <ade-temm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 14:24:41 by ade-temm          #+#    #+#             */
-/*   Updated: 2019/10/11 17:17:27 by ade-temm         ###   ########.fr       */
+/*   Updated: 2019/10/11 17:35:03 by ade-temm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,5 +48,8 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 
 #endif
