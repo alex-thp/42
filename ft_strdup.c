433892951151421f96e5734db86c54be230de4e1
Alexandre DE TEMMERMAN <ade-temm@e1r9p11.42.fr>
2019-10-12 14:24:40 +0200

    deleting errno var

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,12 +6,11 @@
 /*   By: ade-temm <ade-temm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/10 11:11:42 by ade-temm          #+#    #+#             */
-/*   Updated: 2019/10/12 14:16:12 by ade-temm         ###   ########.fr       */
+/*   Updated: 2019/10/12 14:24:04 by ade-temm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <errno.h>
 
 char	*ft_strdup(const char *s1)
 {
@@ -22,10 +21,7 @@ char	*ft_strdup(const char *s1)
 	while (s1[i])
 		i++;
 	if (!(result = malloc(sizeof(char) * (i + 1))))
-	{
-		errno = ENOMEM;
 		return (NULL);
-	}
 	i = 0;
 	while (s1[i])
 	{
